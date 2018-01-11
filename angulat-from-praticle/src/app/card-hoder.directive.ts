d77359e9bcbd29fce6508171667c9681a2f79c32
danilo.agostinho@cedrotech.com <88ab15cd98ef12>
2018-01-11 16:43:17 -0200

    aprendendo HostBind

--- angulat-from-praticle/src/app/card-hoder.directive.ts
+++ angulat-from-praticle/src/app/card-hoder.directive.ts
@@ -1,33 +1,33 @@
 import { CardComponent } from './card/card.component';
-import { element } from 'protractor';
-import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding  } from '@angular/core';
 
 @Directive({
   selector: '[appCardHoder]'
 })
 export class CardHoderDirective {
-
+  // toda vez que sair um evento HostListener atualize o valor da variavel ishovering
+  @HostBinding('class.card-outline-primary') private ishovering: boolean;
   @HostListener('click') onHover() {
     console.log('cliquei!!');
 
     const card = this.elementRef.nativeElement.querySelector('.card-text');
+    const section = this.elementRef.nativeElement.querySelector('.card-section');
 
     this.renderer.setElementStyle(card, 'backgroundColor', 'green');
-  }
+    this.renderer.setElementStyle(section, 'backgroundColor', 'orange');
 
+    this.ishovering = true;
+  }
 
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer) {
-    console.log(elementRef);
     // elementRef.nativeElement.style = 'color: red';
-
-    console.log(renderer);
+    console.log(this.ishovering);
 
     renderer.setElementStyle(elementRef.nativeElement, 'backgroundColor', 'purple');
 
     console.log('depois de ser setado com render', elementRef.nativeElement.style.backgroundColor);
   }
-
 
 }
